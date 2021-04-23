567cc26671488aa35567dbcd47ab65cb1e77ea6a
Terri Squire <codeofarmour@outlook.com>
2021-04-23 21:55:24 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,9 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_ten_times
+  phrase = "Hello World"
+  
+  10.times { puts phrase }
+end
+
+say_hello_ten_times
